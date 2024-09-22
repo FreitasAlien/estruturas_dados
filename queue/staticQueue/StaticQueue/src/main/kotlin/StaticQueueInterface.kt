df0fe2c6802578084d292af70ec978dc652e507b
Gustavo Freitas Rodrigues <gustavofreitasrod@gmail.com>
2024-09-22 19:32:52 -0300

    Create StaticQueueInterface with main and helper methods

---
+++ queue/staticQueue/StaticQueue/src/main/kotlin/StaticQueueInterface.kt
@@ -0,0 +1,12 @@
+interface StaticQueueInterface {
+    //Main Methods
+    fun enqueue(item: Any)
+    fun dequeue(): Any
+    fun front(): Any
+    fun update(item: Any)
+
+    //Helper Methods
+    fun isFull(): Boolean
+    fun isEmpty(): Boolean
+    fun print(): String
+}

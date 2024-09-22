1e72eaaf76d88488a9145ee3547406024ec87141
Gustavo Freitas Rodrigues <gustavofreitasrod@gmail.com>
2024-09-22 16:26:05 -0300

    excluindo

--- pilha/pilhaEstatica/pilhaEstatica/src/main/kotlin/Emplihavel.kt
+++
@@ -1,8 +0,0 @@
-interface Emplihavel {
-    //Metodos Principais
-    fun empilhar(dado: Any) //recebe um dado como parametro e insere no topo da pilha
-    fun desempilhar(dado: Any) //recebe um dado e o remove do topo da pilha
-    
-
-    //Metodos Auxiliares
-}

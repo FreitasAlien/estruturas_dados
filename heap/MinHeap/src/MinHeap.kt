a7a0c8c2f46df64903db3ffb29286b777a0dcb81
Gustavo Freitas Rodrigues <gustavofreitasrod@gmail.com>
2024-12-03 20:12:11 -0300

    Implementing Utility Methods

--- heap/MinHeap/src/MinHeap.kt
+++ heap/MinHeap/src/MinHeap.kt
@@ -23,4 +23,20 @@ class MinHeap(private val size: Int): Heapable {
         }
         return result
     }
+
+    // Utility Methods
+    fun parent(index: Int): Int{
+        return ((index - 1) / 2) // Returns the index of the parent
+    }
+
+    fun leftChild(parentIndex: Int): Int{
+        return (parentIndex * 2 + 1) // Returns the index of the left child
+    }
+
+    fun rightChild(parentIndex: Int): Int{
+        return leftChild(parentIndex) + 1 // Returns the index of the right child
+    }
+
+ ""
+
 }

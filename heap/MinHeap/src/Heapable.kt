2020218ba02da81af92afb6d0879c7f4fe538f02
Gustavo Freitas Rodrigues <gustavofreitasrod@gmail.com>
2024-12-03 19:45:32 -0300

    Heap Interface

---
+++ heap/MinHeap/src/Heapable.kt
@@ -0,0 +1,16 @@
+interface Heapable {
+    //Main Methods
+    fun insert (data: Long)         //C
+    fun getRoot(): Long?            //R
+    fun update(data: Long)          //U
+    fun extract(): Long?            //D
+
+
+
+    //Helper Methods
+    fun isFull(): Boolean
+    fun isEmpty(): Boolean
+    fun printHeap(): String
+
+
+}

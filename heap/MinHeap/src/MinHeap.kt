72f0f861951b8842a43969c6c7d1a37346a1b672
Gustavo Freitas Rodrigues <gustavofreitasrod@gmail.com>
2024-12-03 20:00:11 -0300

    Implementing isFull() and isEmpty()

--- heap/MinHeap/src/MinHeap.kt
+++ heap/MinHeap/src/MinHeap.kt
@@ -4,6 +4,14 @@ class MinHeap(private val size: Int): Heapable {
     private var heapData = LongArray(size){0}
     private var end = -1
 
+    // Implementing Helper Methods
+    override fun isFull(): Boolean {
+        return (end == heapData.size -1) // Returns 'true' if the heap is full
+    }
+
+    override fun isEmpty(): Boolean {
+        return (end == -1) // Returns 'true' if the heap is empty
+    }
 
 
 }

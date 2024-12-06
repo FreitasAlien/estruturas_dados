17e9c4262e45218ab1c93c7dbdd65173d657a958
Gustavo Freitas Rodrigues <gustavofreitasrod@gmail.com>
2024-12-06 03:08:47 -0300

    Implementing Stackable Interface

--- stack/DynamicStack/DynamicStack/src/Stackable.kt
+++ stack/DynamicStack/DynamicStack/src/Stackable.kt
@@ -1,2 +1,12 @@
 interface Stackable {
+    // Main Methods
+    fun push(item: Any?)
+    fun pop(): Any?
+    fun peek(): Any?
+    fun update(item: Any?)
+
+    // Helper Methods
+    fun isEmpty(): Boolean
+    fun isFull(): Boolean
+    fun print(): String
 }

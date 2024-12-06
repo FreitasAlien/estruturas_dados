90f0e28d096dfb21add7d19b28cae0da653ba7e9
Gustavo Freitas Rodrigues <gustavofreitasrod@gmail.com>
2024-12-06 20:23:03 -0300

    Implementing print() method

--- queue/DynamicQueue/DynamicQueue/src/DynamicQueue.kt
+++ queue/DynamicQueue/DynamicQueue/src/DynamicQueue.kt
@@ -13,6 +13,18 @@ class DynamicQueue(private val size: Int): Queueable {
     }
 
     override fun print(): String{
+        var result = "["
+        var aux: Node? = head
+        for(i in 0 until quantity){
+            result += "${aux?.data}"
 
+            if(i == quantity - 1)
+                result += ","
+
+            aux = aux?.previous
+        }
+        return "$result]"
     }
+
+
 }

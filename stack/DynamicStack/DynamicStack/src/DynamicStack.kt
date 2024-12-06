e6341f90e7a6f46d7e997d198111c25c30a00dfa
Gustavo Freitas Rodrigues <gustavofreitasrod@gmail.com>
2024-12-06 03:12:00 -0300

    Implementing Helper Methods

--- stack/DynamicStack/DynamicStack/src/DynamicStack.kt
+++ stack/DynamicStack/DynamicStack/src/DynamicStack.kt
@@ -1,4 +1,27 @@
 class DynamicStack(var size: Int): Stackable{
     private var topPointer: Node? = null
     private var quantity: Int = 0
+
+    // Implementing Helper Methods
+    override fun isEmpty(): Boolean{
+        return (quantity == 0)
+    }
+
+    override fun isFull(): Boolean{
+        return (quantity == size)
+    }
+
+    override fun print(): String{
+        var aux = topPointer
+        var result = "["
+        for(i in 0 until quantity){
+            result += aux?.data
+            if(i != quantity - 1)
+                result += ","
+
+            aux = aux?.previous
+        }
+        return "$result]"
+    }
+
 }

0de7816aa944f8ffae1a6d864433231d93ab9fc2
Gustavo Freitas Rodrigues <gustavofreitasrod@gmail.com>
2024-12-06 22:11:19 -0300

    Implementing isEmpty() and isFull()

--- list/DynamicList/DynamicList/src/DynamicList.kt
+++ list/DynamicList/DynamicList/src/DynamicList.kt
@@ -3,4 +3,18 @@ class DynamicList(private var size: Int): Listable {
     private var head: Node? = null
     private var tail: Node? = null
     private var quantity: Int = 0
+
+    // Helper Methods
+    override fun isEmpty(): Boolean{
+        return (quantity == 0)
+    }
+
+    override fun isFull(): Boolean{
+        return (quantity == size)
+    }
+
+    override fun print(): String{
+
+    }
+
 }

c3e46f2acff483fc42741faf146a3228b7ac06c7
Gustavo Freitas Rodrigues <gustavofreitasrod@gmail.com>
2024-12-06 22:02:59 -0300

    Implementing listable interface

--- list/DynamicList/DynamicList/src/Listable.kt
+++ list/DynamicList/DynamicList/src/Listable.kt
@@ -1,2 +1,15 @@
 interface Listable {
+    // Main Methods
+    fun append(item: Any?)
+    fun insert(index: Int, item: Any?)
+    fun get(index: Int): Any?
+    fun getAll(): Array<Any?>
+    fun update(index: Int, item: Any?)
+    fun delete(index: Int): Any?
+    fun clear()
+
+    // Helper Methods
+    fun isEmpty(): Boolean
+    fun isFull(): Boolean
+    fun print(): String
 }
